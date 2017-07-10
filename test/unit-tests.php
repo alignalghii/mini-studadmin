c069d779ad614e220bd56a6a1325586e335b6499
Mark Endrey <ungazik@gmail.com>
2017-07-10 13:38:47 +0200

    Test `Either` + Add some reporting functionalities to test runner

--- test/unit-tests.php
+++ test/unit-tests.php
@@ -4,10 +4,33 @@ require '../autoload.php';
 
 use framework\Util;
 use framework\AlgebraicDataTypes\Maybe;
+use framework\AlgebraicDataTypes\Either;
 
+function statusMsg($status) {return $status ? 'OK' : 'Wrong';}
+function printStatus($status) {echo statusMsg($status) . "\n";}
+
+$allStatus = true;
+
+echo "Inspected functionalities:\n";
+
+echo ' - framework\Util::array_mapMaybe_access_keys: ';
 $r = Util::array_mapMaybe_access_keys(function($k, $v) {return Maybe::just($k + $v);}, [10,20,30]);
 $status = $r === [10, 21, 32];
-echo $status ? 'OK' : 'Wrong';
-echo "\n";
+printStatus($status);
+$allStatus = $allStatus && $status;
 
+echo ' - framework\AlgebraicDataTypes\Either: ';
+$leftDiv0 = Either::left('Division by zero');
+$right5   = Either::right(5);
+function reportEither($eitherErrorLabelOrNumResult)
+{
+	return $eitherErrorLabelOrNumResult->either(
+		function ($errorLabel) {return "Problem: $errorLabel";},
+		function ($numResult)  {return "The value is: $numResult";}
+	);
+}
+$status = reportEither($right5) == 'The value is: 5' && reportEither($leftDiv0) == 'Problem: Division by zero';
+printStatus($status);
 
+$allStatus = $allStatus && $status;
+echo "-------> Overall status: " . statusMsg($allStatus) . "\n";
