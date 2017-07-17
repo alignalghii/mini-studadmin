e4b2fc1b459f1c5109fe881e072c08d91beaffd2
Mark Endrey <ungazik@gmail.com>
2017-07-17 18:58:06 +0200

    Linking source code and living hosted application together

---
+++ README.md
@@ -0,0 +1,2 @@
+An embryonic microframework experiment.
+The microframeowork is expempified by a small sample web application (not much more yet than a CRUD). You can see [it hosted living here](http://web.studentadministrationframework.nhely.hu/).
--- app/View/Home/index.php
+++ app/View/Home/index.php
@@ -3,3 +3,4 @@
 			<li><a class="menu-icon" href="/study-group">Study groups list</a></li>
 		</ul>
 		<h1><?php echo $title; ?></h1>
+		This is a sample custom miniframework sample. You can see <a href="https://github.com/alignalghii/mini-studadmin">its sourcecode on GitHub</a>.
