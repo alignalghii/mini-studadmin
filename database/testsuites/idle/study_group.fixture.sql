d921afaf90a616c67a2ae840a015006937f8aeef
Mark Endrey <ungazik@gmail.com>
2017-08-24 21:12:34 +0200

    Minor change: typo in database data

--- database/testsuites/idle/study_group.fixture.sql
+++ database/testsuites/idle/study_group.fixture.sql
@@ -5,5 +5,5 @@ INSERT INTO `study_group`
 ( 3,   'Moonwatchers',     'Dr Jane Adams',    'astronomy', '2011-05-27 03:04:14'),
 ( 4,   'Stinkies',         'Dr Judy Bliss',    'chemistry', '2000-04-13 22:11:01'),
 ( 5,   'Kitchen wonders',  'Dr Tom Ron',       'physics',   '1999-07-12 08:06:07'),
-( 6,   'Powerpuff ladies', 'Dr heather Black', 'sociology', '2008-12-06 11:45:30'),
+( 6,   'Powerpuff ladies', 'Dr Heather Black', 'sociology', '2008-12-06 11:45:30'),
 ( 7,   'Cricket and ant',  'Dr Pete Rodman',   'economy',   '2014-11-29 16:15:02');
