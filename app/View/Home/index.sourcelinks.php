dc8b879fd01d262c0a614b84c48905d66d47a800
Mark Endrey <ungazik@gmail.com>
2017-08-27 20:34:48 +0200

    Sourcelinks for GET /, i.e. HomeController::index

--- app/View/Home/index.sourcelinks.php
+++ app/View/Home/index.sourcelinks.php
@@ -1,7 +1,15 @@
-		<ul class="upper-menu external">
-			<li><a class="menu-icon" href="https://github.com/alignalghii/mini-studadmin">GitHub repository</a></li>
-			<li><a class="menu-icon" href="https://github.com/alignalghii/mini-studadmin/blob/master/app/Controller/HomeController.php">Source file of the concerned controller</a></li>
-		</ul>
+		<table class="tree">
+			<tr>
+				<th colspan="9" title="alignalghii/mini-studadmin"><a class="source" href="https://github.com/alignalghii/mini-studadmin" target="_blank">GitHub repository</a></td>
+			</tr>
+			<tr>
+				<td rowspan="2" title="GET /"><a class="source" href="https://github.com/alignalghii/mini-studadmin/blob/92efdbce271416373b116ec348a40d30e216a2e2/app/Routes.php#L20" target="_blank">Route match</a></td>
+				<td rowspan="2" title="called at &hellip;">&rarr;</td>
+				<td rowspan="2" title="HomeController::index"><a class="source" href="https://github.com/alignalghii/mini-studadmin/blob/92efdbce271416373b116ec348a40d30e216a2e2/app/Controller/HomeController.php#L12-L17" target="_blank">Controller action</a></td>
+				<td title="called at &hellip;">&rarr;</td>
+				<td title="Home/index" class="full"><a class="source" href="https://github.com/alignalghii/mini-studadmin/blob/92efdbce271416373b116ec348a40d30e216a2e2/app/View/Home/index.php" target="_blank">View</a></td>
+			</tr>
+		</table>
 		<ul class="upper-menu internal">
 			<li><a class="menu-icon" href="/student">Students list</a></li>
 			<li><a class="menu-icon" href="/study-group">Study groups list</a></li>
