e908ab821d5df650359aae00dfa2ba6462a52729
Mark Endrey <ungazik@gmail.com>
2017-08-28 18:20:28 +0200

    Good intro text on Home page

--- app/View/Home/index.php
+++ app/View/Home/index.php
@@ -1,3 +1,17 @@
 <?php require '../app/View/Home/index.sourcelinks.php'; ?>
 		<h1><?php echo $title; ?></h1>
-		This is a sample custom miniframework sample. You can see <a href="https://github.com/alignalghii/mini-studadmin">its sourcecode on GitHub</a>.
+		This is an educational site for web programming with some narrow topics.
+		Technically, the site itself is a tiny web application, barely surpassing the level of a simple CRUD.
+		But there is a self-made web application framework underlying the sample application. This provides the real topics of the site.
+		<h2>Topics</h2>
+		How to implement and use a <a href="">standalone tiny embryonic web application framework</a>.
+		<ul>
+			<li>Loose coupling. It presents either familiar ad hoc techniques in a somewhat distilled form, or some standard ideas implemented in a simplified form. Some of these ideas come from <a href="http://symfony.com/">Symfony</a>.</li>
+			<li>How to use ideas from declarative, functional programming paradigm in web programming, and how to implemet them in PHP. Most ideas are taken from <a href="https://www.haskell.org/">Haskell</a> world, and terminologies follow the conventions there.</li>
+		</ul>
+		There is also another goal and feature of this site &mdash; <a href="">experimenting with a self-documenting content structure</a>:
+		<ul>
+			<li>Provide a sourcecode link structure for each functionality just in place on the user interface.</li>
+			<li>For each functionality, arrange the sourcode link structure according to a call tree, maybe refined to represent the logics and architecture.</li>
+		</ul>
+		<h2>Contents, site map</h2>
