94c50823bc192a3e0e12c31e730d44c3d998107c
Mark Endrey <ungazik@gmail.com>
2017-08-29 22:11:39 +0200

    Bootstrap document subpage sourcelinks

--- app/View/Home/sourcelinks.php
+++ app/View/Home/sourcelinks.php
@@ -3,9 +3,9 @@
 				<th colspan="9" title="alignalghii/mini-studadmin"><a class="source" href="https://github.com/alignalghii/mini-studadmin" target="_blank">GitHub repository</a></td>
 			</tr>
 			<tr>
-				<td rowspan="2" title="GET /doc/<?php echo $pageName; ?>"><a class="source" href="https://github.com/alignalghii/mini-studadmin/blob/92efdbce271416373b116ec348a40d30e216a2e2/app/Routes.php#L20" target="_blank">Route match</a></td>
+				<td rowspan="2" title="GET /doc/<?php echo $pageName; ?>"><a class="source" href="https://github.com/alignalghii/mini-studadmin/blob/3d6cd09f34d39d9acca1b55de65e8aafb3312e5c/app/Routes.php#L22" target="_blank">Route match</a></td>
 				<td rowspan="2" title="called at &hellip;">&rarr;</td>
-				<td rowspan="2" title="HomeController::doc('<?php echo $pageName; ?>')"><a class="source" href="https://github.com/alignalghii/mini-studadmin/blob/92efdbce271416373b116ec348a40d30e216a2e2/app/Controller/HomeController.php#L12-L17" target="_blank">Controller action</a></td>
+				<td rowspan="2" title="HomeController::doc('<?php echo $pageName; ?>')"><a class="source" href="https://github.com/alignalghii/mini-studadmin/blob/3d6cd09f34d39d9acca1b55de65e8aafb3312e5c/app/Controller/HomeController.php#L27-L37" target="_blank">Controller action</a></td>
 				<td title="called at &hellip;">&rarr;</td>
 				<td title="Home/<?php echo $pageName; ?>" class="full"><a class="source" href="https://github.com/alignalghii/mini-studadmin/blob/master/app/View/Home/<?php echo $pageName; ?>.php" target="_blank">View</a></td>
 			</tr>
